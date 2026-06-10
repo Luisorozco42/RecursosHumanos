11785e5c468151e2a57df59387bcbbd023e2f55d
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-10 15:54:55 -0600

    controler created with a create product method

---
+++ src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,30 @@
+package gm.rh.controlador;
+
+import gm.rh.modelo.Empleado;
+import gm.rh.servicio.EmpleadoServicio;
+import gm.rh.servicio.IEmpleadoServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("rh-app")
+@CrossOrigin(value = "http://localhost:3000")
+public class EmpleadoControlador {
+    @Autowired
+    private IEmpleadoServicio empleadoServicio;
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    @GetMapping("/empleados")
+    public List<Empleado> listarEmpleados() {
+        var empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado) -> logger.info(empleado.getNombre()));
+        return empleados;
+    }
+}
